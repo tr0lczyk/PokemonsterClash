fd7dcce05cc7b08f8bb64dcf246bb452029ff43c
tr0lczyk <matt.olczyk@gmail.com>
2018-01-05 22:04:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/pokemonsterclash/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.pokemonsterclash;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
